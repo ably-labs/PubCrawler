58b838a565a5f72ec011da2ba102d087a9bfdb27
ikbal kaya <ceikbal@gmail.com>
2021-09-16 13:53:15 +0100

    New Android project with empty Activity

---
+++ app/src/main/java/com/ablylabs/pubcrawler/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ablylabs.pubcrawler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
